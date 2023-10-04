1007ec31afdea1d39e7ecbfbd17f229f1c77b8cd
xavitooo <a21xavcabmil@inspedralbes.cat>
2023-10-04 08:50:51 +0200

    Principi de projecte

---
+++ app/src/main/java/com/example/projecte0salmar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.projecte0salmar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textLorem);
+        Button boto = (Button) findViewById(R.id.button1);
+
+        boto.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/projecte0salmar/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.projecte0salmar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        text = (TextView) findViewById(R.id.titol);
+        Button boto = (Button) findViewById(R.id.opt1);
+
+        boto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
